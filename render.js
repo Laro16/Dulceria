b0505367179ea1b96cb9ed2f4ac8e8b4ca384af5
Laro16 <luis21aro@gmail.com>
2025-09-22 12:38:23 -0600

    Update render.js

--- render.js
+++ render.js
@@ -1,8 +1,11 @@
 // render.js - monta el componente en #root usando React 18
 const rootEl = document.getElementById('root');
-if (rootEl) {
+if (rootEl && window.ReactDOM && window.DulceriaApp) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(React.createElement(window.DulceriaApp));
+} else {
+  console.error('Render error: falta ReactDOM o DulceriaApp');
 }
+
 
 
